9b63eba47bbd26989df2cb830f80e1ced438e48a
guishijin <420188751@qq.com>
2018-07-12 12:15:47 +0800

    增加注释

--- app/src/main/java/com/ylsk/androidacc/BaseSensorActivity.java
+++ app/src/main/java/com/ylsk/androidacc/BaseSensorActivity.java
@@ -8,20 +8,34 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+/**
+ * 传感器基础窗口类
+ */
 public abstract class BaseSensorActivity extends Activity implements SensorEventListener {
+
     private static final String TAG = "BaseSensorActivity";
+
+    // 传感器管理器
     protected SensorManager mSensorManager;
+    // 传感器对象
     protected Sensor mSensor;
+
+    // 传感器采集延时
     protected int mSensorDelay;
 
+    /**
+     * 初始构造函数
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 根据传感器类型获取传感器对象---传感器类型由子类重写
         try{
             mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
             mSensor = mSensorManager.getDefaultSensor(getDefaultSensor());//sensor type
             mSensorDelay = getDefaultSensorDelay(); // 获取采集延时 默认为：SensorManager.SENSOR_DELAY_GAME 20毫秒一个采集周期
-
         } catch (Exception ex) {
             Toast.makeText(this, "检测失败！", Toast.LENGTH_SHORT).show();
         } catch (Error error) {
@@ -29,6 +43,9 @@ public abstract class BaseSensorActivity extends Activity implements SensorEvent
         }
     }
 
+    /**
+     * 恢复处理
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -42,6 +59,9 @@ public abstract class BaseSensorActivity extends Activity implements SensorEvent
         }
     }
 
+    /**
+     * 暂停处理
+     */
     @Override
     protected void onPause() {
         super.onPause();
@@ -52,14 +72,31 @@ public abstract class BaseSensorActivity extends Activity implements SensorEvent
         }
     }
 
+    /**
+     * 实现传感器接口，由子类实现
+     * @param sensorEvent
+     */
     @Override
     public abstract void onSensorChanged(SensorEvent sensorEvent);
 
+    /**
+     * 实现传感器接口，由子类实现
+     * @param sensor
+     * @param i
+     */
     @Override
     public abstract void onAccuracyChanged(Sensor sensor, int i);
 
+    /**
+     * 获取默认的传感器，由子类实现
+     * @return
+     */
     protected abstract int getDefaultSensor();
 
+    /**
+     * 获取传感器延时，默认为GAME等级
+     * @return
+     */
     protected int getDefaultSensorDelay()
     {
         return SensorManager.SENSOR_DELAY_GAME;
--- app/src/main/java/com/ylsk/androidacc/DeviceSensorInfoActivity.java
+++ app/src/main/java/com/ylsk/androidacc/DeviceSensorInfoActivity.java
@@ -9,24 +9,51 @@ import android.widget.TextView;
 
 import java.util.List;
 
+/**
+ * 手机传感器详细信息
+ */
 public class DeviceSensorInfoActivity extends Activity {
+
     private static final String TAG = "DeviceSensorInfo";
+
+    // 文本框
     private TextView mTvInfo;
 
+    /**
+     * 构造函数
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_sensor_info);
+
+        // 获取文本框
         mTvInfo = (TextView)findViewById(R.id.tv_info);
+
+        // 获取传感器管理器
         SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // 使用传感器管理器获取所有类型的传感器的列表
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        mTvInfo.setText("sensors: " + sensors.size());
-        Log.d(TAG, "sensors: " + sensors.size());
+
+        // 输出传感器总个数
+        mTvInfo.setText("传感器总个数: " + sensors.size());
+        Log.d(TAG, "传感器总个数: " + sensors.size());
+
+        // 循环遍历每一个传感器
         for (int i = 0;i < sensors.size();++i) {
+
+            // 输出传感器
+            Log.d(TAG,"\n--------------------------------------------");
+            Log.d(TAG,"sensor type: "+sensors.get(i).getType()+" | " + sensors.get(i).getStringType());
             Log.d(TAG,"sensor name: "+sensors.get(i).getName());
             Log.d(TAG,"sensor vendor: "+sensors.get(i).getVendor());
             Log.d(TAG,"sensor power: "+sensors.get(i).getPower());
             Log.d(TAG, "sensor resolution: " + sensors.get(i).getResolution());
+
+            mTvInfo.append("\n--------------------------------------------");
+            mTvInfo.append("\n"+i+": sensor type: "+sensors.get(i).getType()+" | " + sensors.get(i).getStringType());
             mTvInfo.append("\n"+i+": sensor name: " + sensors.get(i).getName());
             mTvInfo.append("\n"+i+": sensor vendor: "+sensors.get(i).getVendor());
             mTvInfo.append("\n"+i+": sensor power: "+sensors.get(i).getPower());
